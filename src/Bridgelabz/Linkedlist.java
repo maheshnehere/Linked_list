1c1d43309e22407df67e5bb2d333292e28db37a0
maheshnehere <maheshnehere93@gmail.com>
2023-03-13 18:22:21 +0530

    Added:Add first method used linkedlist

--- src/Bridgelabz/Linkedlist.java
+++ src/Bridgelabz/Linkedlist.java
@@ -14,39 +14,35 @@ public class Linkedlist {
         }
     }
 
-    //Uc1 = Create simple Linked List.
-    public void NaturalLinkedList(Object data) {
-        Node newNode = new Node(data); // create object of Node and pushing data.
-        if (head == null) {
-            head = newNode;     // if linkedList is empty then new node will be head.
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;  // traversing the temp till end of the list.
-        }
-        temp.next = newNode;
+   //Uc2 = Create Linked list by using add First method.
+   public void AddFirst(int data){
+    Node newNode  = new Node(data);
+    if (head == null) {                // first checking head is null or not
+        head = newNode;                // if head is null then new node will be the head.
+        return;
     }
-
-    public void print() {
-        if (head == null) {
-            System.out.println("Linked List is Empty.");
-            return;
-        }
-        Node temp = head;        // taking temp as a temporary variable to keep head as a constant.
-        while (temp != null) {
-
-            System.out.print(temp.data + " => "); // It will print all the data in the list.
-            temp = temp.next;
-        }
-        System.out.println("Null");
+    newNode.next = head;
+    head = newNode;
+}
+// Display the methods.
+public void print() {
+    if (head == null) {
+        System.out.println("Linked List is Empty.");
+        return;
     }
+    Node temp = head;               // taking temp as a temporary variable to keep head as a constant.
+    while (temp != null) {
 
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.NaturalLinkedList(85);
-        list.NaturalLinkedList(24);
-        list.NaturalLinkedList(16);
-        list.print();
+        System.out.print(temp.data + " => "); // It will print all the data in the list.
+        temp = temp.next;
     }
+    System.out.println("Null");
+}
+public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.AddFirst(85);
+    list.AddFirst(24);
+    list.AddFirst(16);
+    list.print();
+}
 }
